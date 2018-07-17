636adddbaff3111ffa0e0dd57eb9c3cf149afae6
Magdalena Miszewska <mag.miszewska@gmail.com>
2018-07-17 12:53:23 +0200

    Create routes.js, update content in files

--- src/index.js
+++ src/index.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
 import DevTools from './DevTools';
 import { getCountries } from '../src/actions/actions-countries';
-import Navigation from './presentational/navigation.component';
+import routes from './routes';
 
 render(
     <Provider store={store}>
-        <Router history={hashHistory}>
-            <Route path='/' component={Navigation}>
-                <div>
-                    <Navigation />
-                    <h1>Inicjalizacja projektu</h1>
-                    <DevTools />
-                </div>
-            </Route>
-        </Router>
+        <Router history={hashHistory} routes={routes}/>
     </Provider>,
-    document.getElementById('root')    
+    document.getElementById('root')
 );
 
 store.dispatch(getCountries());
