c5166e6ebbb5ab16e2505242c65534baee7a4a62
Magdalena Miszewska <mag.miszewska@gmail.com>
2018-07-17 12:46:45 +0200

    Create navigation component, add routing

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
 import DevTools from './DevTools';
 import { getCountries } from '../src/actions/actions-countries';
+import Navigation from './presentational/navigation.component';
 
 render(
     <Provider store={store}>
-        <div>
-            <h1>Inicjalizacja projektu</h1>
-            <DevTools />
-        </div>
+        <Router history={hashHistory}>
+            <Route path='/' component={Navigation}>
+                <div>
+                    <Navigation />
+                    <h1>Inicjalizacja projektu</h1>
+                    <DevTools />
+                </div>
+            </Route>
+        </Router>
     </Provider>,
-    document.getElementById('root')
+    document.getElementById('root')    
 );
 
 store.dispatch(getCountries());
