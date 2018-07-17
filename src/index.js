15fa568bfce1d3707e6d542da604da5e374e7f3f
Magdalena Miszewska <mag.miszewska@gmail.com>
2018-07-17 12:58:25 +0200

    Create three presentational components - not-found, contact and home

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { render } from 'react-dom';
 import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
-import DevTools from './DevTools';
 import { getCountries } from '../src/actions/actions-countries';
 import routes from './routes';
 
